ecde42493cbde88f9e52c0baf279be419a932d3b
Anders Bakken <agbakken@gmail.com>
2014-09-03 19:05:49 -0700

    Start some work on clang detection.

--- src/Compiler.cpp
+++ src/Compiler.cpp
@@ -35,29 +35,6 @@ void Compiler::ensureEnviron()
     }
 }
 
-static Path::VisitResult visitor(const Path &path, void *userData)
-{
-    Set<Path> &files = *reinterpret_cast<Set<Path> *>(userData);
-    if (path.isSymLink()) {
-        if (path.isFile()) {
-            Path link = path.followLink();
-            link.resolve(Path::RealPath, path.parentDir());
-            visitor(link, userData);
-        }
-    } else if (path.isDir()) {
-        return Path::Recurse;
-    } else if (path.mode() & 0111) {
-        char *buf = 0;
-        const int read = path.readAll(buf, 3);
-        if (read != 3 || strncmp(buf, "#!/", read))
-            files.insert(path);
-        delete[] buf;
-    } else if (path.contains(".so")) {
-        files.insert(path);
-    }
-    return Path::Continue;
-}
-
 std::shared_ptr<Compiler> Compiler::compiler(const Path &compiler, const String &path)
 {
     ensureEnviron();
@@ -74,47 +51,7 @@ std::shared_ptr<Compiler> Compiler::compiler(const Path &compiler, const String 
         return c;
     }
     SHA256 sha;
-#if 0
-    Process process;
-    List<String> environment;
-    if (!path.isEmpty()) {
-        environment = sEnviron;
-        environment += ("PATH=" + path);
-    }
-    if (!process.exec(compiler, List<String>() << "-v" << "-E" << "-", environment)) {
-        error() << "Failed to package compiler" << compiler << path;
-        return std::shared_ptr<Compiler>();
-    }
-
-    Set<String> compilerPaths;
-    const List<String> lines = process.readAllStdErr().split('\n');
-    String version;
-    for (int i=0; i<lines.size(); ++i) {
-        const String &line = lines.at(i);
-        sha.update(line);
-
-        if (compilerPaths.isEmpty() && line.size() > 14 && line.startsWith("COMPILER_PATH=")) {
-            compilerPaths = line.mid(14).split(':').toSet();
-        } else if (version.isEmpty() && line.size() > 12 && line.startsWith("gcc version")) {
-            int space = line.indexOf(' ', 12);
-            if (space == -1)
-                space = line.size();
-
-            version = line.mid(12, space - 12);
-            warning() << "Compiler version" << version;
-        }
-    }
-
-    if (!compilerPaths.isEmpty()) {
-        c.reset(new Compiler);
-        for (Set<String>::const_iterator it = compilerPaths.begin(); it != compilerPaths.end(); ++it) {
-            const Path p = Path::resolved(*it);
-            if (p.isDir()) {
-                p.visit(visitor, &c->mFiles);
-            }
-        }
-    }
-#else
+#ifdef OS_Linux
     c.reset(new Compiler);
     List<String> shaList;
     {
@@ -175,6 +112,7 @@ std::shared_ptr<Compiler> Compiler::compiler(const Path &compiler, const String 
         sha.update(s);
         error() << s;
     }
+#elif defined(OS_Darwin)
 
 #endif
     if (c) {
