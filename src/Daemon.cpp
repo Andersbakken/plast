111727b12e9c49551e836fa825903cc1a069126d
Jan Erik Hanssen <jhanssen@gmail.com>
2014-11-09 22:53:13 -0800

    better

--- src/Daemon.cpp
+++ src/Daemon.cpp
@@ -651,16 +651,10 @@ int Daemon::startCompileJobs()
             if (ret == -1) {
                 error() << "Unable to make fifo" << errno;
             } else {
-                job->tempObjectFd = open(job->tempObjectFile.constData(), O_RDONLY);
+                eintrwrap(job->tempObjectFd, open(job->tempObjectFile.constData(), O_RDONLY|O_NONBLOCK));
                 if (job->tempObjectFd == -1) {
                     error() << "Unable to open fifo" << errno;
                 } else {
-                    int flags;
-                    eintrwrap(flags, fcntl(job->tempObjectFd, F_GETFL, 0));
-                    if (flags != -1) {
-                        int err;
-                        eintrwrap(err, fcntl(job->tempObjectFd, F_SETFL, flags | O_NONBLOCK));
-                    }
                     EventLoop::eventLoop()->registerSocket(job->tempObjectFd, EventLoop::SocketRead,
                                                            [job](int fd, unsigned int) {
                                                                char buf[8192];
