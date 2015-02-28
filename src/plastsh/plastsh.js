1c1420d2545a127042bba1a94b184dea1386d201
Jan Erik Hanssen <jhanssen@gmail.com>
2015-02-28 02:10:36 -0800

    more

--- src/plastsh/plastsh.js
+++ src/plastsh/plastsh.js
@@ -1,7 +1,31 @@
 /*global require, process console*/
 
+var opts = {
+    host: "127.0.0.1",
+    port: 8089
+};
+
 var readline = require('readline');
-var ws = require('ws');
+var websocket = require('ws');
+var ws;
+
+function setupWs()
+{
+    ws = new websocket('ws://' + opts.host + ':' + opts.port + '/');
+
+    ws.on('message', function(data, flags) {
+        console.log('ws msg', data);
+        rl.prompt();
+    });
+    ws.on('close', function() {
+        console.log('ws socket closed');
+        process.exit(0);
+    });
+    ws.on('error', function(e) {
+        console.log('ws error', e);
+        process.exit();
+    });
+}
 
 var handlers = {
     quit: function() {
@@ -34,6 +58,13 @@ var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+(function(argv) {
+    opts.host = argv.host || argv.h || opts.host;
+    opts.port = parseInt(argv.port) || parseInt(argv.p) || opts.port;
+})(require('minimist')(process.argv.slice(2)));
+
+setupWs();
 
 rl.setPrompt('plastsh> ');
 rl.prompt();
