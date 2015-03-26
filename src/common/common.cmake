8c6f220b114f9ec2e49b1049f0604a828234e9bd
Jan Erik Hanssen <jhanssen@gmail.com>
2015-03-25 23:23:34 -0700

    Make it possible to build outside of the source directory

--- src/common/common.cmake
+++ src/common/common.cmake
@@ -12,6 +12,6 @@ if (APPLE)
 endif()
 
 # find_library(WSLAY_LIBRARY wslay HINTS ${CMAKE_CURRENT_LIST_DIR}/../lib)
-target_link_libraries(common -L${CMAKE_CURRENT_LIST_DIR}/../lib -lwslay -Wl,-rpath,${CMAKE_CURRENT_LIST_DIR}/../lib)
+target_link_libraries(common -L${CMAKE_CURRENT_BINARY_DIR}/wslay-install/lib -lwslay -Wl,-rpath,${CMAKE_CURRENT_LIST_DIR}/../lib)
 
-include_directories(${CMAKE_CURRENT_LIST_DIR} ${CMAKE_CURRENT_LIST_DIR}/../json/src)
+include_directories(${CMAKE_CURRENT_LIST_DIR} ${CMAKE_CURRENT_BINARY_DIR}/wslay-install/include ${CMAKE_CURRENT_LIST_DIR}/../json/src)
